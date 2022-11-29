9c4ed51a2b183034990dc191a673ff583e48e390
Victor Salles <victor.salles@engenharia.ufjf.br>
2022-11-29 17:41:58 -0300

    Implementada a função para redirecionar para a pag /profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function Header({ pageName }) {
+function Header({ pageName, history }) {
   return (
     <div>
       <h1 data-testid="page-title">
         {pageName}
         {' '}
       </h1>
-      <button type="button">
+      <button
+        type="button"
+        onClick={ () => { history.push('/profile'); } }
+      >
         <img
           src={ profileIcon }
           data-testid="profile-top-btn"
           alt="Ícone de Perfil"
         />
       </button>
-      <button type="button">
+      <button
+        type="button"
+      >
         <img
           src={ searchIcon }
           data-testid="search-top-btn"
@@ -30,6 +35,9 @@ function Header({ pageName }) {
 
 Header.propTypes = {
   pageName: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default Header;
