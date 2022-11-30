0c75ac1e8e4950d2ca62a12b46e7884738e134f9
Yanni <yannidilly.prog@gmail.com>
2022-11-30 15:46:19 -0300

    adiciona botões a página

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -15,7 +15,25 @@ function Profile() {
   return (
     <div>
       <h1>Profile</h1>
-      <p>{ email }</p>
+      <p data-testid="profile-email">{ email }</p>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Logout
+      </button>
     </div>
   );
 }
