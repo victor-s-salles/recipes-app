d44d0e39e18e054948b52b40aabeeabb0f8896f6
Victor Salles <victor.salles@engenharia.ufjf.br>
2022-12-01 15:46:29 -0300

    Req 51 e 52 feitos

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,48 +1,41 @@
 import React from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
-  const receitas = [{
-    id: '1234',
-    type: 'meal',
-    nationality: 'Brasil',
-    category: 'categoria',
-    alcoholicOrNot: 'alcoólica',
-    name: 'carne',
-    image: 'foto',
-  }, {
-    id: '1000',
-    type: 'meal',
-    nationality: 'Brasil',
-    category: 'categoria',
-    alcoholicOrNot: '',
-    name: 'frango',
-    image: 'foto2',
-  }];
+  // const receitas = [
+  //   {
+  //     id: '1234',
+  //     type: 'meal',
+  //     nationality: 'Italian',
+  //     category: 'Vegetarian',
+  //     alcoholicOrNot: '',
+  //     name: 'Spicy Arrabiata Penne',
+  //     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  //   },
+  //   {
+  //     id: '1000',
+  //     type: 'drink',
+  //     nationality: 'brasil',
+  //     category: 'cate',
+  //     alcoholicOrNot: 'Yes',
+  //     name: 'frango',
+  //     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+  //   },
+  // ];
   return (
     <div>
       <Header pageName="Favorite Recipes" searchingOFF />
       <h1>Lista de receitas favoritas</h1>
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-      >
+      <button type="button" data-testid="filter-by-all-btn">
         All
-
       </button>
-      <button
-        type="button"
-        data-testid="filter-by-meal-btn"
-      >
+      <button type="button" data-testid="filter-by-meal-btn">
         Meals
-
       </button>
-      <button
-        type="button"
-        data-testid="filter-by-drink-btn"
-      >
+      <button type="button" data-testid="filter-by-drink-btn">
         Drinks
-
       </button>
 
       {receitas.map((e, index) => (
@@ -52,46 +45,36 @@ function FavoriteRecipes() {
             alt={ e.name }
             data-testid={ `${index}-horizontal-image` }
           />
-          <p
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {e.category}
-
-          </p>
-          <p
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {e.name}
-
-          </p>
-          <p
-            data-testid={ `${index}-horizontal-done-date` }
-          >
+          <p data-testid={ `${index}-horizontal-top-text` }>{e.category}</p>
+          <p data-testid={ `${index}-horizontal-name` }>{e.name}</p>
+          {e.type === 'meal' && (
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {`${e.nationality} - ${e.category}`}
+            </p>
+          )}
+          <p data-testid={ `${index}-horizontal-done-date` }>
             DATA QUE A RECEITA FOI FEITA
-
           </p>
+          {e.type === 'drink' && (
+            <p data-testid={ `${index}-horizontal-top-text` }>{e.alcoholicOrNot}</p>
+          )}
           <button
+            src={ shareIcon }
             type="button"
             data-testid={ `${index}-horizontal-share-btn` }
           >
-            COMPARTILHAR
-
+            <img src={ shareIcon } alt="Share button" />
           </button>
-          <p
-            data-testid={ `${index}-${e.type}-horizontal-tag` }
-          >
-            {e.type}
-
-          </p>
+          <p data-testid={ `${index}-${e.type}-horizontal-tag` }>{e.type}</p>
           <button
+            src={ blackHeartIcon }
             type="button"
             data-testid={ `${index}-horizontal-favorite-btn` }
           >
-            FAVORITAR
+            <img src={ blackHeartIcon } alt="Favorite button" />
           </button>
-
         </div>
-      )) }
+      ))}
     </div>
   );
 }
