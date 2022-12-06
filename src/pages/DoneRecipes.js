b6a1f9428a2c4bd18527e4230422deab8a96bf8c
Leonardo Guedes Ferreira <leoguedes99@gmail.com>
2022-12-06 18:46:00 -0300

    Validaçao para LocalStorage sem DoneRecipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -6,8 +6,17 @@ import Header from '../components/Header';
 function DoneRecipes() {
   const [recipeType, setRecipeType] = useState('all');
   const [actualRecipe, setActualRecipe] = useState('');
+  const [localEmpty, setLocalEmpty] = useState(true);
+
+  const isEmpty = () => {
+    const recipes = localStorage.getItem('doneRecipes');
+    if (recipes) {
+      setLocalEmpty(false);
+    }
+  };
 
   useEffect(() => {
+    isEmpty();
     const doneRecipes = localStorage.getItem('doneRecipes');
     switch (recipeType) {
     case 'all':
@@ -23,73 +32,80 @@ function DoneRecipes() {
       break;
     }
   }, [recipeType]);
+
   return (
     <div>
       <Header pageName="Done Recipes" searchingOFF />
-      <button
-        type="button"
-        data-testid="filter-by-all-btn"
-        onClick={ () => setRecipeType('all') }
-      >
-        All
-      </button>
-      <button
-        type="button"
-        data-testid="filter-by-meal-btn"
-        onClick={ () => setRecipeType('meal') }
-      >
-        Meals
-      </button>
-      <button
-        data-testid="filter-by-drink-btn"
-        type="button"
-        onClick={ () => setRecipeType('drink') }
-      >
-        Drinks
-      </button>
-      {actualRecipe.map((recipe, index) => (
-        <div key={ index }>
-          <Link to={ `/drinks/${recipe.id}` }>
-            <img
-              src={ recipe.image }
-              alt={ `${recipe.name} imagem` }
-              data-testid={ `${index}-horizontal-img` }
-            />
-            <h2
-              data-testid={ `${index}-horizontal-name` }
-            >
-              {recipe.name}
-            </h2>
-          </Link>
-          {recipe.type === meal ? (
-            <h3
-              data-testid={ `${index}-horizontal-top-text` }
-            >
-              {`${recipe.nationality} - ${recipe.category}`}
-            </h3>)
-
-            : (
-              <h3
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                {recipe.alcoholicOrNot}
-              </h3>
-            )}
-          <h2
-            data-testid={ `${index}-horizontal-done-date` }
+      {!localEmpty ? (
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+            onClick={ () => setRecipeType('all') }
           >
-            {recipe.doneDate}
-          </h2>
-          {recipe.type === 'meal' ? (
-            <h2 data-testid={ `${index}-${recipe.tag}-horizontal-tag` }>
-              {recipe.tag[0]}
-              {recipe.tag[1]}
-            </h2>)
-            : null }
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-meal-btn"
+            onClick={ () => setRecipeType('meal') }
+          >
+            Meals
+          </button>
+          <button
+            data-testid="filter-by-drink-btn"
+            type="button"
+            onClick={ () => setRecipeType('drink') }
+          >
+            Drinks
+          </button>
+          {actualRecipe.map((recipe, index) => (
+            <div key={ index }>
+              <Link to={ `/drinks/${recipe.id}` }>
+                <img
+                  src={ recipe.image }
+                  alt={ `${recipe.name} imagem` }
+                  data-testid={ `${index}-horizontal-img` }
+                />
+                <h2
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {recipe.name}
+                </h2>
+              </Link>
+              {recipe.type === meal ? (
+                <h3
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`${recipe.nationality} - ${recipe.category}`}
+                </h3>)
 
-        </div>
+                : (
+                  <h3
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    {recipe.alcoholicOrNot}
+                  </h3>
+                )}
+              <h2
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {recipe.doneDate}
+              </h2>
+              {recipe.type === 'meal' ? (
+                <h2 data-testid={ `${index}-${recipe.tag}-horizontal-tag` }>
+                  {recipe.tag[0]}
+                  {recipe.tag[1]}
+                </h2>)
+                : null }
 
-      ))}
+            </div>
+
+          ))}
+
+        </div>)
+        : <h1>Nenhuma receita foi finalizada </h1> }
+
     </div>
   );
 }
