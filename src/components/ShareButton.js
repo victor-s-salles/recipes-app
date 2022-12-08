5f6c5971ea8894f09c14d90ed5448a3b7691d5b0
Victor Salles <victor.salles@engenharia.ufjf.br>
2022-12-08 18:08:12 -0300

    Resolvido o 40, e alterações no 33

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import Copy from 'clipboard-copy';
+import { useLocation } from 'react-router-dom';
 
 function ShareButton() {
-  const url = window.location.href.toString();
+  const location = useLocation();
+
+  let url = `http://localhost:3000${location.pathname}`;
+  console.log(url.split('/in-progress')[0]);
+
+  if (url.includes('in-progress')) {
+    const newUrl = url.split('/in-progress')[0];
+    url = newUrl;
+  }
   const [isCopied, setIsCopied] = useState(false);
   const onCopyButtonClick = () => {
     Copy(url);
